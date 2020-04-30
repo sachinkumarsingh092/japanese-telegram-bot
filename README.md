210dc1f888b7246f337b8ad491aa7e8bd36964b0
Sachin Kumar Singh <sachinkumarsingh092@gmail.com>
2020-04-30 16:11:19 +0530

    Added features

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-Japanese Telegram bot
+# Japanese Telegram bot
+
+This bot is used to translate any language query to Japanese.
+This is especially useful for people trying to learn japanese. Use /help option to get more details.
+
+## ToDo
+[] inline mode
+[] group inclusion
--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from gtts import gTTS
 from bs4 import BeautifulSoup
-import json, urllib.request, re, requests, os
+import json, urllib.request, urllib.parse, re, requests, os
 
 import logging
 import configparser
@@ -9,15 +9,23 @@ from telegram.ext.dispatcher import run_async
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, InlineQueryHandler
 from telegram import ChatAction,ParseMode, InlineQueryResultArticle, InputTextMessageContent
 
-BOTNAME = "JapaneseBot"
 
+
+BOTNAME = "speakJapaneseBot"
+
+
+
+# used when sending message to many users simultaneously, like in groups
 @run_async
 def send_async(bot, *args, **kwargs):
     bot.sendMessage(*args, **kwargs)
 
 
+
+
+# decorator function for bot typing action
 def send_typing_action(func):
-    """Sends typing action while processing func command."""
+    """ Sends typing action while processing func command."""
 
     @wraps(func)
     def command_func(update, context, *args, **kwargs):
@@ -27,78 +35,119 @@ def send_typing_action(func):
     return command_func
 
 
+
+# loging utilities
 logging.basicConfig(level=logging.DEBUG, format='%(levelname)s - %(message)s')
 logger = logging.getLogger()
 
+
+
+# Updater objects
 updater = Updater(token=os.environ['token'], use_context=True)
 dispatcher = updater.dispatcher
 
-@send_typing_action
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Use this bot to learn Japanese!")
 
 
-# text = "i live in london. I love playing table tennis.\
-#     This is so much fun."
 
-# ## scrapping 
-# # url = "https://translate.google.com/#view=home&op=translate&sl=auto&tl=ja&text="+text
-# # page = requests.get(url)
-# # soup = BeautifulSoup(page.content, 'html.parser')
-# # titles = soup.findAll('div', {'class': 'tlid-transliteration-content.transliteration-content.full'})
-# # print(titles)
-
+# function to parse the contents of the user's input
 def parse(update, context):
-    text=""
-    for _ in context.args:
-        temp = re.sub('[^A-Za-z0-9 ]+', '', _)
-        text += temp + ' '
-    text = re.sub('\.', '+', text)
-    ttemp = text.split()
-    # print(temp)
-    sourceText = '+'.join(ttemp)
-    print(sourceText)
-    return sourceText
+    ''' Parse the argumensts '''
+    text = ' '.join(context.args)
 
-@send_typing_action
-def fetch(update, context):
+    # convert the argument in standard uri format
+    url_args=urllib.parse.quote(text)
+    return url_args
+
+
+
+def convert(update, context):
+    ''' translating wrapper fro /translate '''
     base_url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=ja&dt=t&q="
     full_url = base_url+parse(update, context)
-    print(full_url)
-    with urllib.request.urlopen(full_url) as response:
-        data = json.load(response)    
-        print(data[0][0][0])
-    output = gTTS(text=data[0][0][0], lang='ja', slow=False) 
-    output.save("output.mp3")
-    context.bot.send_message(chat_id=update.effective_chat.id, text=data[0][0][0])
-    context.bot.send_voice(chat_id=update.effective_chat.id, voice=open('output.mp3', 'rb'))
+    # print(full_url)
 
-# def inline(update, context):
+    # make request to the api to get the json file
+    with urllib.request.urlopen(full_url) as response:
+        data = json.load(response)
+        response.close()
+        text=""
+        for i in range(len(data[0])):
+            # print(data[0][i][0])
+            text += data[0][i][0]
+    
+    return text
+
+
+
+# /start option
+@send_typing_action
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Konichiwa.\
+         Query me some text for translation. Use /help whenever stuck")
+
+
+
+# /translate option
+@send_typing_action
+def translate(update, context):
+    ''' Translate the user's input and make a translated output audio '''
+    text = convert(update, context)
+
+    # translate using gtts module
+    output = gTTS(text=text, lang='ja', slow=False) 
+    output.save("output/output.mp3")
+
+    # reply a text and an audio translated output
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+    context.bot.send_voice(chat_id=update.effective_chat.id, voice=open('output/output.mp3', 'rb'))
+
+
+# /help option
+@send_typing_action
+def help(update, context):
+    ''' Help message '''
+    context.bot.send_message(chat_id=update.effective_chat.id, text='''Use the following commands
+/start - start message
+/translate - translate the message into an audio file
+/help - to get this help meassage
+''')
+
+####### TO DO
+# def inline_caps(update, context):
 #     query = update.inline_query.query
+
 #     if not query:
 #         return
 #     results = list()
-#     text=parse(update, context)
 #     results.append(
 #         InlineQueryResultArticle(
 #             id=query.upper(),
-#             title='Inline',
-#             input_message_content=InputTextMessageContent(text)
+#             title='Translate',
+#             input_message_content=InputTextMessageContent(convert(update, context))
 #         )
 #     )
-   # context.bot.answer_inline_query(update.inline_query.id, results)
+#     context.bot.answer_inline_query(update.inline_query.id, results)
+
 
 
 @send_typing_action
 def unknown(update, context):
+    ''' Manage any unexpected input '''
     context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
 
 
+
+# Dispatchers
 dispatcher.add_handler(CommandHandler('start', start))
-dispatcher.add_handler(CommandHandler('fetch', fetch))
-# dispatcher.add_handler(InlineQueryHandler(inline))
+dispatcher.add_handler(CommandHandler('translate', translate))
+dispatcher.add_handler(CommandHandler('help', help))
+# dispatcher.add_handler(InlineQueryHandler(inline_caps))
 dispatcher.add_handler(MessageHandler(Filters.command, unknown))
 
 
+# start the bot
 updater.start_polling()
+
+
+# Stop the bot until Ctrl+C is pressed or a signal is sent to the bot process
 updater.idle()
