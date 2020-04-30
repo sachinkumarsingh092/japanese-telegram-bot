e3f124b7ee472ba9fdb89276d90eb158bf71cf4c
Sachin Kumar Singh <sachinkumarsingh092@gmail.com>
2020-04-30 21:24:40 +0530

    Added changes

--- bot.py
+++ bot.py
@@ -150,7 +150,7 @@ dispatcher.add_handler(MessageHandler(Filters.command, unknown))
 
 
 # start the bot
-updater.start_polling()
+updater.start_polling(poll_interval=1.0, timeout=20)
 
 
 # Stop the bot until Ctrl+C is pressed or a signal is sent to the bot process
